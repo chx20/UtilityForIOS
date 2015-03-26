88192c3ca1da9a6e06ff5847177273b69417c991
chx20 <394468254@qq.com>
2015-03-26 14:47:56 +0800

    Initial Commit

---
+++ UtilityForIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UtilityForIOS
+//
+//  Created by 程红秀 on 2015-3-26.
+//  Copyright (c) 2015年 chx20. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UtilityForIOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UtilityForIOS
+//
+//  Created by 程红秀 on 2015-3-26.
+//  Copyright (c) 2015年 chx20. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
